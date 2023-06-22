81af5b49445595a4606a1560ffd6a9e4f683233a
ES <ES@ES-KOMPUTEREMSZ>
2023-06-22 18:48:02 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie = "Emilia";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Jestem Emilia");
+        }
+    }
+
+
+}
+
+
